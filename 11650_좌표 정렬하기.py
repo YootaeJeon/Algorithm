92d7bd1fd17a57a3115a6232a959cb2f17d0f733
Jeon_YT <jeon_lovely@kakao.com>
2026-03-11 14:15:08 +0900

    alg: 2026-03-11 14:15

--- 11650_좌표 정렬하기.py
+++ 11650_좌표 정렬하기.py
@@ -11,6 +11,18 @@
 2 2
 3 3
 3 4
+
+# 틀렸음 -> 자료형 때문
+n = int(input())
+arr = []
+for i in range(n):
+    arr.append(input().split()) # 좌표가 문자열 리스트로 저장
+    # 문자열 정렬은 숫자 크기가 아니라 문자 순서로 비교합니다.
+arr.sort()
+
+for i in arr:
+    print(' '.join(i))
+
 '''
 
 n = int(input())
