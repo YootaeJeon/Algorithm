65053271081c5ab85844f399114bb6a0410f29cc
Jeon_YT <jeon_lovely@kakao.com>
2026-03-11 17:00:01 +0900

    alg: 2026-03-11 17:00

--- 10814_나이순정렬.py
+++ 10814_나이순정렬.py
@@ -1,3 +1,4 @@
+'''
 n = int(input())
 
 age_name_arr = []
@@ -13,3 +14,23 @@ age_name_arr.sort(key=lambda x: (x[1], x[0]))
 for w in age_name_arr:
     print(w[1], end=" ")
     print(w[2])
+    
+
+정답은 맞음
+GPT 풀이
+'''
+    
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = []
+
+for _ in range(n):
+    age, name = input().split()
+    arr.append((int(age), name))
+
+arr.sort(key=lambda x: x[0])
+
+for age, name in arr:
+    print(age, name)
