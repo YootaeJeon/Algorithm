8c4c19715be47ec1cd23f953431bfd95610d1eb4
Jeon_YT <jeon_lovely@kakao.com>
2026-03-18 16:55:06 +0900

    alg: 2026-03-18 16:55

--- 4134_베르트랑 공준.py
+++ 4134_베르트랑 공준.py
@@ -1,29 +1,30 @@
-'''
-조건으로 제어
-n = int(input())
+import sys
+input = sys.stdin.readline
 
-while n != 0:
-    print("test")
-    n = int(input())
-'''
+MAX = 246912
 
-def is_prime(x):
-    if x < 2:
-        return False
-    for i in range(2, int(x ** 0.5) + 1):
-        if x % i == 0:
-            return False
-    return True
+# 에라토스테네스의 체
+prime = [True] * (MAX + 1)
+prime[0] = prime[1] = False
+
+for i in range(2, int(MAX ** 0.5) + 1):
+    if prime[i]:         #시작 # 끝  # 증가 단위
+        for j in range(i * i, MAX + 1, i):
+            prime[j] = False
 
 while True:
     n = int(input())
-    count = 0
     if n == 0:
         break
     
-    
-    for i in range(n+1, 2*n + 1):
-        if is_prime(i):
-            count+=1
-    
+    count = 0
+    for i in range(n + 1, 2 * n + 1):
+        if prime[i]:
+            count += 1
+            
     print(count)
+    
+# i =2, > 4,6,8.... false 제거됨 ...
+# i =3, > 9, 12 ,15...false 3 소수 ...
+# i = 4  이미 제거되서 실행 x 
+# i= 5, > 25, 30, 35,40..false 5소수...
