95ad24ecbd81ac7d87b7c4cd36d6fe6f2ac0ad14
Jeon_YT <jeon_lovely@kakao.com>
2026-03-26 14:22:16 +0900

    alg: 2026-03-26 14:22

--- 28278_ 스택 2.py
+++ 28278_ 스택 2.py
@@ -24,6 +24,36 @@
 #     elif cmd == 4 : # 비엇으면 1, 아니면 0
 #         if Arr == Null
 
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+stack = []
+
+for _ in range(n):
+    cmd = input().split()
+    
+    if cmd[0] == '1':  # push
+        stack.append(int(cmd[1]))
+        
+    elif cmd[0] == '2':  # pop
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+            
+    elif cmd[0] == '3':  # size
+        print(len(stack))
+        
+    elif cmd[0] == '4':  # empty
+        print(0 if stack else 1)
+        
+    elif cmd[0] == '5':  # top
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
+
 
 '''
 
